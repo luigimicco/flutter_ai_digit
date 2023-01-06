c671a7c57fb0d4de941d106556b4f6bfffb45838
luigimicco <luigi.micco@gmail.com>
2023-01-06 12:33:10 +0100

    add draw_screen

---
+++ lib/screens/draw_screen.dart
@@ -0,0 +1,105 @@
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+
+import '../utils/constants.dart';
+import 'drawing_painter.dart';
+
+class DrawScreen extends StatefulWidget {
+  const DrawScreen({super.key});
+
+  @override
+  State<DrawScreen> createState() => _DrawScreenState();
+}
+
+class _DrawScreenState extends State<DrawScreen> {
+  final List<Offset?> _points = [];
+  bool initialize = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Digit Recognizer'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: const <Widget>[
+                      Text(
+                        'MNIST database of handwritten digits',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'The digits have been size-normalized and centered in a fixed-size images (28 x 28)',
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          _drawCanvasWidget(),
+          const SizedBox(
+            height: 10,
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.delete),
+        onPressed: () {
+          setState(() {
+            _points.clear();
+          });
+        },
+      ),
+    );
+  }
+
+  Widget _drawCanvasWidget() {
+    return Container(
+      width: Constants.canvasSize + Constants.borderSize * 2,
+      height: Constants.canvasSize + Constants.borderSize * 2,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.black,
+          width: Constants.borderSize,
+        ),
+      ),
+      child: GestureDetector(
+        onPanUpdate: (DragUpdateDetails details) {
+          Offset localPosition = details.localPosition;
+          if (localPosition.dx >= 0 &&
+              localPosition.dx <= Constants.canvasSize &&
+              localPosition.dy >= 0 &&
+              localPosition.dy <= Constants.canvasSize) {
+            setState(() {
+              _points.add(localPosition);
+            });
+          }
+        },
+        onPanEnd: (DragEndDetails details) {
+          _points.add(null);
+          // _recognize();
+        },
+        child: CustomPaint(
+          painter: DrawingPainter(_points),
+        ),
+      ),
+    );
+  }
+}
